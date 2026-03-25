c884e885f3d225323eb556e5a8875f27b8ead904
Leaf-life <77422322+Leaf-life@users.noreply.github.com>
2026-03-24 23:12:52 -0600

    started work on ServerFacade

---
+++ server/src/main/java/server/ServerFacade.java
@@ -0,0 +1,100 @@
+package server;
+
+import com.google.gson.Gson;import passoff.exception.ResponseParseException;
+import java.net.http.HttpClient;
+
+import io.javalin.*;
+import io.javalin.http.Context;
+import model.*;
+import service.*;
+import dataaccess.*;
+
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ServerFacade {
+
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public void registration() throws ResponseParseException{
+
+    }
+
+    public void login() throws ResponseParseException{
+
+    }
+
+    public void logout() throws ResponseParseException{
+
+    }
+
+    public void listGames() throws ResponseParseException{
+        var request = buildRequest("GET", "/pet", null);
+        var response = sendRequest(request);
+        return handleResponse(response, listGames.class);
+    }
+
+    public void createGame() throws ResponseParseException{
+
+    }
+
+    public void joinGame() throws ResponseParseException{
+
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws DataAccessException{
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
